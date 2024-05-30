d4401e3eb86c0fe7c3dafa633cbda91929d3f0e7
Hem Pun <hembdrpun10@gmail.com>
2024-05-30 12:45:50 +0545

    Migrate to new DB and Fix Minor BUG

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"],
+  publicRoutes: ["/api/:path*", "/"],
+  // ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/"],
 });
 
 export const config = {
